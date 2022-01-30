c68ed0e530c9065d3dfefc9ab52b047ff31bc97a
quezia-lima <qfonseca1@gmail.com>
2022-01-29 22:20:03 -0300

    Requisito 2: corrige texto referente ao atributo alt da imagem

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -21,7 +21,7 @@ describe('Teste o componente `<About.js />.`', () => {
   test('- Teste se a página contém a seguinte imagem de uma Pokédex: `https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png`', () => {
     renderWithRouter(<About />);
 
-    const image = screen.getByRole('img', { name: /pokédex/i });
+    const image = screen.getByRole('img', { name: /Pokédex/i });
     expect(image).toBeDefined();
     expect(image).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
